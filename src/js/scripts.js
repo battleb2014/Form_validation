ba8c0a2f2452f05155aa5c922eaa1ff6e6de952c
Brandon <battleb2014@gmail.com>
2019-09-06 14:29:04 -0400

    more changes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,9 @@
-function validate() {
-  var username = getElementById('username').value;
-  var password = getElementById('password').value;
+var usernameArr = [];  //store usernames
+var passwordArr = [];  //store passwords
+var usernameInput = document.getElementById('username');
+var passwordInput = document.getElementById('password');
 
-    if (email == 'Email' || password == 'Password') {
-      document.getElementById('result').innerHTML = 'All fields required';
-      return false;
-    }  else
-      if (password.length < 6){
-      document.getElementById('result').innerHTML = alert('Password must be at least 6 characters long');
-    } else {
-      return true;
-    }
+function insert() = {
+    usernameArr.push( usernameInput.value );
+    passwordArr.push( passwordInput.value );
 };
-if(validate = true) {
-    var submit = '../html/p2.html';
-}
-
-document.getElementById('signIn').onclick = validate();
